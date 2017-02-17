42d396401803bf123bbd5e52707e4c046e296ba3
xmithd <amit.hdayaram@gmail.com>
2017-02-16 21:25:29 -0500

    Added/modified entity classes

--- src/main/java/ca/concordia/pivottable/entities/DataType.java
+++ src/main/java/ca/concordia/pivottable/entities/DataType.java
@@ -1,9 +1,21 @@
 package ca.concordia.pivottable.entities;
 
+/**
+ * Some data types: numeric and string for now.
+ * Internally represented as string.
+ */
 public enum DataType {
-    TYPE_INTEGER,
-    TYPE_STRING,
-    TYPE_REAL,
-    TYPE_DATE,
-    TYPE_LONG
+    TYPE_NUMERIC("numeric"),
+    TYPE_STRING("string");
+
+    private final String val;
+
+    private DataType(final String val) {
+        this.val = val;
+    }
+
+    @Override
+    public String toString() {
+       return this.val;
+    }
 }
