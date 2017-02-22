033c9deef79cafe4654f1e59da59927e900b3134
xmithd <amit.hdayaram@gmail.com>
2017-02-21 19:59:52 -0500

    Completing DBConnectionCheckController

--- src/main/java/ca/concordia/pivottable/controller/DBConnectionCheckController.java
+++ src/main/java/ca/concordia/pivottable/controller/DBConnectionCheckController.java
@@ -2,6 +2,7 @@ package ca.concordia.pivottable.controller;
 
 import ca.concordia.pivottable.servicelayer.DataRetrievalService;
 import ca.concordia.pivottable.utils.DependenciesContainer;
+import com.google.gson.Gson;
 import spark.Request;
 import spark.Response;
 
@@ -26,6 +27,18 @@ public class DBConnectionCheckController extends Controller {
     public Object handle(Request request, Response response) {
         log.info("Called the DB Connection check controller");
         // TODO
-        return "[\"Hello\"]";
+        String dataSourceName = request.params("dataSourceName");
+        String username = request.params("username");
+        String password = request.params("password");
+        boolean isConnectionGood = dataRetrievalService.checkDataSourceConnection(dataSourceName, username, password);
+        if (isConnectionGood) {
+            String result = "Connection is good";
+            response.status(200);
+            return result;
+        } else {
+           String result = "No good";
+           response.status(401);
+           return result;
+        }
     }
 }
