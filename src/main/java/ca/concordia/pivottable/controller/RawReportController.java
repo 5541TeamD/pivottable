5e32a9927facc1fba12b5b18ed58626b4e1a34ad
Annabelle Williams <annabelle.e.williams@gmail.com>
2017-03-04 14:41:05 -0500

    Modified RawReportController - errors remain

--- src/main/java/ca/concordia/pivottable/controller/RawReportController.java
+++ src/main/java/ca/concordia/pivottable/controller/RawReportController.java
@@ -1,11 +1,18 @@
 package ca.concordia.pivottable.controller;
 
+import ca.concordia.pivottable.servicelayer.DataRetrievalService;
 import ca.concordia.pivottable.utils.DependenciesContainer;
+import ca.concordia.pivottable.entities.DataSet;
 import spark.Request;
 import spark.Response;
 
+
 public class RawReportController extends Controller{
 
+    /**
+     * Services needed
+     */
+    private DataRetrievalService dataRetrievalService;
 
     /**
      * All controllers need a constructor that needs the container as argument.
@@ -13,13 +20,26 @@ public class RawReportController extends Controller{
      * @param container The DI container
      */
     public RawReportController(DependenciesContainer container) {
+
         super(container);
+        dataRetrievalService = container.get("dataRetrievalService");
     }
 
     @Override
     public Object handle(Request request, Response response) throws Exception {
         //return new Object();
         log.info("Hello!");
-        return "Request to handle raw report data was received.";
+        String name;
+        boolean rawreportexists = DataRetrievalService.rawReportExists(name);
+        if (rawreportexists) {
+            DataSet rawReport = DataRetrievalService.getRawReport(request);
+            response.status(200);
+            return rawReport;
+        }else{
+            log.info("Raw report does not exist.");
+            response.status(404);
+            return null;
+        }
+
     }
 }
