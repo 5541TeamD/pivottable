a4ca7bfa32dd1033ccf28686c3265bea6285f735
Annabelle Williams <annabelle.e.williams@gmail.com>
2017-03-06 19:49:12 -0500

    finished TableListController-Testing needed

--- src/main/java/ca/concordia/pivottable/controller/TableListController.java
+++ src/main/java/ca/concordia/pivottable/controller/TableListController.java
@@ -1,14 +1,18 @@
 package ca.concordia.pivottable.controller;
 
+import ca.concordia.pivottable.servicelayer.DataRetrievalService;
 import ca.concordia.pivottable.utils.DependenciesContainer;
+import com.google.gson.Gson;
 import spark.Request;
 import spark.Response;
+import java.util.List;
+
 
 /**
  * Handles the request to get the list of tables
  */
 public class TableListController extends Controller {
-
+    private DataRetrievalService dataRetrievalService;
 
     /**
      * All controllers need a constructor that needs the container as argument.
@@ -16,13 +20,23 @@ public class TableListController extends Controller {
      * @param container The DI container
      */
     public TableListController(DependenciesContainer container) {
+
         super(container);
+        dataRetrievalService = container.get("dataRetrievalService");
     }
 
     @Override
     public Object handle(Request request, Response response) throws Exception {
         //return new Object();
-        log.info("Hello!");
-        return "Request to handle list of tables was received.";
+        log.info("TableListController was called.");
+        List<String> tableList = dataRetrievalService.getAllRawReportNames();
+        response.status(200);
+        response.header("Content-Type", "application/json");
+        return toJson(tableList);
+    }
+
+    private String toJson(List<String> tableList) {
+        Gson gson = new Gson();
+        return gson.toJson(tableList);
     }
 }
